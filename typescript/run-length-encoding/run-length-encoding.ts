73ecb6615e0763476877cab72942817dbff8acfd
Sven Kroell <sven.kroell@olx.com>
2019-03-29 13:41:26 +0100

    add run length encoding

--- typescript/run-length-encoding/run-length-encoding.ts
+++ typescript/run-length-encoding/run-length-encoding.ts
@@ -3,15 +3,12 @@ class RunLengthEncoding {
         return stringToEncode.replace(/(.)\1*/g, (matches, letter) => {
             const length = matches.length;
             const lengthString = (length === 1) ? "" : length
-            if (letter === " ") {
-                return matches
-            }
             return `${lengthString}${letter}`
         })
     }
 
     static decode(stringToDecode: string): string {
-        return stringToDecode.replace(/(\d+)(\w)/g, (
+        return stringToDecode.replace(/(\d+)([a-zA-Z ])/g, (
             _,
             numberOfRepetitions,
             character
